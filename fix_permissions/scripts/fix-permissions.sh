447522ec8b8b9a01d380dee824224166dc95de0f
Christopher Gervais <chris@ergonlogic.com>
2016-08-30 14:59:32 +0000

    Revise script docs, argument names and permission style to more closely match existing Aegir standards.

--- fix_permissions/scripts/fix-permissions.sh
+++ fix_permissions/scripts/fix-permissions.sh
@@ -3,87 +3,88 @@
 # Help menu
 print_help() {
 cat <<-HELP
-This script is used to fix permissions of a Drupal installation
-you need to provide the following arguments:
+This script is used to fix the file ownership and permissions of a Drupal
+platform. You need to provide the following arguments:
 
-  1) Path to your Drupal installation.
-  2) Username of the user that you want to give files/directories ownership.
-  3) HTTPD group name (defaults to www-data for Apache).
+  --root: Path to the root of your Drupal installation.
+  --script-user: Username of the user to whom you want to give
+      files/directories ownership (defaults to 'aegir').
+  --web-group: Web server group name (defaults to 'www-data').
 
-Usage: (sudo) bash ${0##*/} --drupal_path=PATH --drupal_user=USER --httpd_group=GROUP
-Example: (sudo) bash ${0##*/} --drupal_path=/usr/local/apache2/htdocs --drupal_user=john --httpd_group=www-data
+Usage: (sudo) ${0##*/} --root=PATH --script-user=USER --web_group=GROUP
+Example: (sudo) ${0##*/} --drupal_path=/var/aegir/platforms/drupal-7.50 --script-user=aegir --web-group=www-data
 HELP
 exit 0
 }
 
 if [ $(id -u) != 0 ]; then
-  printf "**************************************\n"
-  printf "* Error: You must run this with sudo or root*\n"
-  printf "**************************************\n"
+  printf "***********************************************\n"
+  printf "* Error: You must run this with sudo or root. *\n"
+  printf "***********************************************\n"
   print_help
   exit 1
 fi
 
-drupal_path=${1%/}
-drupal_user=${2}
-httpd_group="${3:-www-data}"
+drupal_root=${1%/}
+script_user=${2:-aegir}
+web_group="${3:-www-data}"
 
 # Parse Command Line Arguments
 while [ "$#" -gt 0 ]; do
   case "$1" in
-    --drupal_path=*)
-        drupal_path="${1#*=}"
+    --root=*)
+        drupal_root="${1#*=}"
         ;;
-    --drupal_user=*)
-        drupal_user="${1#*=}"
+    --script-user=*)
+        script_user="${1#*=}"
         ;;
-    --httpd_group=*)
-        httpd_group="${1#*=}"
+    --web-group=*)
+        web_group="${1#*=}"
         ;;
     --help) print_help;;
     *)
-      printf "***********************************************************\n"
+      printf "************************************************************\n"
       printf "* Error: Invalid argument, run --help for valid arguments. *\n"
-      printf "***********************************************************\n"
+      printf "************************************************************\n"
       exit 1
   esac
   shift
 done
 
-if [ -z "${drupal_path}" ] || [ ! -d "${drupal_path}/sites" ] || [ ! -f "${drupal_path}/core/modules/system/system.module" ] && [ ! -f "${drupal_path}/modules/system/system.module" ]; then
-  printf "*********************************************\n"
-  printf "* Error: Please provide a valid Drupal path. *\n"
-  printf "*********************************************\n"
+if [ -z "${drupal_root}" ] || [ ! -d "${drupal_root}/sites" ] || [ ! -f "${drupal_root}/core/modules/system/system.module" ] && [ ! -f "${drupal_root}/modules/system/system.module" ]; then
+  printf "********************************************************\n"
+  printf "* Error: Please provide a valid Drupal root directory. *\n"
+  printf "********************************************************\n"
   print_help
   exit 1
 fi
 
-if [ -z "${drupal_user}" ] || [[ $(id -un "${drupal_user}" 2> /dev/null) != "${drupal_user}" ]]; then
-  printf "*************************************\n"
+if [ -z "${script_user}" ] || [[ $(id -un "${script_user}" 2> /dev/null) != "${script_user}" ]]; then
+  printf "***************************************\n"
   printf "* Error: Please provide a valid user. *\n"
-  printf "*************************************\n"
+  printf "***************************************\n"
   print_help
   exit 1
 fi
 
-cd $drupal_path
-printf "Changing ownership of all contents of "${drupal_path}":\n user => "${drupal_user}" \t group => "${httpd_group}"\n"
-chown -R ${drupal_user}:${httpd_group} .
+cd $drupal_root
+printf "Changing ownership of all contents of "${drupal_root}":\n user => "${script_user}" \t group => "${web_group}"\n"
+chown -R ${script_user}:${web_group} .
 
-printf "Changing permissions of all directories inside "${drupal_path}" to "rwxr-x---"...\n"
-find . -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+printf "Changing permissions of all directories inside "${drupal_root}" to "750"...\n"
+find . -type d -exec chmod 750 '{}' \;
 
-printf "Changing permissions of all files inside "${drupal_path}" to "rw-r-----"...\n"
-find . -type f -exec chmod u=rw,g=r,o= '{}' \;
+printf "Changing permissions of all files inside "${drupal_root}" to "640"...\n"
+find . -type f -exec chmod 640 '{}' \;
 
-printf "Changing permissions of "files" directories in "${drupal_path}/sites" to "rwxrwx---"...\n"
+printf "Changing permissions of "files" directories in "${drupal_root}/sites" to "770"...\n"
 cd sites
-find . -type d -name files -exec chmod ug=rwx,o= '{}' \;
+find . -type d -name files -exec chmod 770 '{}' \;
 
-printf "Changing permissions of all files inside all "files" directories in "${drupal_path}/sites" to "rw-rw----"...\n"
-printf "Changing permissions of all directories inside all "files" directories in "${drupal_path}/sites" to "rwxrwx---"...\n"
+printf "Changing permissions of all files inside all "files" directories in "${drupal_root}/sites" to "660"...\n"
+printf "Changing permissions of all directories inside all "files" directories in "${drupal_root}/sites" to "770"...\n"
 for x in ./*/files; do
-  find ${x} -type d -exec chmod ug=rwx,o= '{}' \;
-  find ${x} -type f -exec chmod ug=rw,o= '{}' \;
+  find ${x} -type f -exec chmod 660 '{}' \;
+  find ${x} -type d -exec chmod 770 '{}' \;
 done
 echo "Done setting proper permissions on files and directories"
